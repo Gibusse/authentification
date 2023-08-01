71ff30a78f7e8255348a7ba524fced55f5008737
Pierre II MAUGER <pierreii.mauger@cgi.com>
2023-08-01 12:37:33 -0400

    Add login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginPageComponent,
+    loadChildren: () => import('./login-page/login-page.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: '',
+    component: AppComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
