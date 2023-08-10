94dbd0da6ea80676df819a26cfcc475cc5234029
Pierre II MAUGER <pierreii.mauger@cgi.com>
2023-08-10 15:28:25 -0400

    Add lint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found.component';
-import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
   {
