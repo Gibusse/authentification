9de3fd67b8f7948385be1ae7c97afb4d5d26ab12
Pierre II MAUGER <pierreii.mauger@cgi.com>
2023-08-17 14:10:11 -0400

    feat : AUTH-0002 Add authentification with database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared-module.module';
 import { NotFoundComponent } from './not-found.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AUTHSERVICE } from './core/tokens';
+import { environment } from 'src/environments/environment';
+import { OAuthModule } from 'angular-oauth2-oidc';
 
 @NgModule({
   declarations: [
@@ -13,10 +17,22 @@ import { NotFoundComponent } from './not-found.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    OAuthModule.forRoot({
+      resourceServer: {
+        allowedUrls: ['https://billing_demo_api.com/api'],
+        sendAccessToken: true
+      }
+    }),
     SharedModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AUTHSERVICE, useClass: environment.authProvider
+    },
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
